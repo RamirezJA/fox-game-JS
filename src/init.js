42a3897c8460856aae87bdfedc849e742dd87629
Dredge <26813102+RamirezJA@users.noreply.github.com>
2020-09-13 11:45:06 -0700

    gameState made :octocat:

---
+++ src/init.js
@@ -0,0 +1,28 @@
+//not working try rm -rf .cache dist/ 
+
+const TICK_RATE = 3000;
+
+function tick() {
+  console.log("tick", Date.now());
+}
+
+async function init() {
+  console.log('starting game');
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
